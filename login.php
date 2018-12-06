49e4ce0ac9309731df69da82481c7ca19fe0611b
Benoit LEGER <benoit.leger@hotmail.fr>
2018-12-06 22:04:32 +0100

    login

---
+++ login.php
@@ -0,0 +1,35 @@
+<?php
+
+session_start();
+header('Content-type: application/json');
+
+if (isset($_POST) && isset($_POST['email']) && isset($_POST['password'])) {
+    // Attempt login
+    if (login($_POST['email'], $_POST['password'])) {
+       
+    } else {
+        // Login has failed
+    }
+}
+
+function hashPassword($password) {
+    return md5('sel' . $password . 'sel');
+}
+
+function login($email, $password) {
+
+    $user = getUserEmail($email);
+
+    // If user exists
+    if ($user != null) {
+        if (hashPassword($password) == $user['password']) {
+            // Connect successfull
+            $_SESSION['id_user'] = $user['id_user'];
+            return true;
+        } else {
+            return false;
+        }
+    } else {
+        return false;
+    }
+}
